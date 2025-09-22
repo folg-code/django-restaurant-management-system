78eafdc2d38beaced53627bdc51f1c711373cfe2
filip <filip1folga@gmail.com>
2025-09-22 13:52:45 +0200

    implement base.html

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from kitchen.models import DishType, Cook, Dish
 
 # Create your views here.
+def index(request):
+    num_dish_type = DishType.objects.count()
+    num_cook = Cook.objects.count()
+    num_dish = Dish.objects.count()
+
+    context = {
+        "num_dish_type": num_dish_type,
+        "num_cook": num_cook,
+        "num_dish": num_dish,
+    }
+
+    return render(request, "kitchen/index.html", context)
