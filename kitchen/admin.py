e6645cd06a0d5c3ff6c97f1cf77fbb2fccdec33e
filip <filip1folga@gmail.com>
2025-09-22 17:05:27 +0200

    Add admin panel with DishType, Dish, and Cook model registrations

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import DishType, Dish, Cook
 
-# Register your models here.
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "dish_type", "price")
+    list_filter = ("dish_type",)
+    search_fields = ("name",)
+
+
+@admin.register(Cook)
+class CookAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "first_name", "last_name", "years_of_experience")
+    search_fields = ("username", "first_name", "last_name")
--- kitchen/models.py
+++ kitchen/models.py
@@ -8,7 +8,7 @@ class DishType(models.Model):
         return self.name
 
 class Cook(AbstractUser):
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(default=0)
 
 class Dish(models.Model):
     name = models.CharField(max_length=100)
