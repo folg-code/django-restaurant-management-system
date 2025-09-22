631a736b04e5e21e1eab64c282f768ed6b29b2b8
filip <filip1folga@gmail.com>
2025-09-22 14:01:32 +0200

    Set args to DecimalField

--- kitchen/models.py
+++ kitchen/models.py
@@ -13,7 +13,7 @@ class Cook(AbstractUser):
 class Dish(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
     cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='dishes')
 
