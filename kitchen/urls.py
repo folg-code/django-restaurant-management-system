cd5432a4dde6d5a960d69e8a674f6f5156b6759d
filip <filip1folga@gmail.com>
2025-09-24 10:09:01 +0200

    create urls for views

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,6 +1,42 @@
 from django.urls import path
-from kitchen import views
+from kitchen.views import (
+    index,
+    DishTypeListView,
+    DishListView,
+    DishDetailView,
+    CookListView,
+    CookDetailView
+    )
+
+from . import views
+from .views import index
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+
+    path(
+        "dishes/",
+        views.DishListView.as_view(),
+        name="dish-list",
+    ),
+    path(
+        "dishes/<int:pk>/",
+        views.DishDetailView.as_view(),
+        name="dish-detail",
+    ),
+    path(
+        "cooks/",
+        views.CookListView.as_view(),
+        name="cook-list",
+    ),
+    path(
+        "cooks/<int:pk>/",
+        views.CookDetailView.as_view(),
+        name="cook-detail",
+    ),
+    path(
+        "dish_types/",
+        views.DishTypeListView.as_view(),
+        name="dish_type-list",
+    ),
 ]
