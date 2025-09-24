71016ac4c594db8b02339dd5709f7743338bb0c1
filip <filip1folga@gmail.com>
2025-09-24 12:14:55 +0200

    add @login_required, LoginRequiredMixin to views

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -11,32 +11,34 @@ from kitchen.views import (
 from . import views
 from .views import index
 
+
+app_name = "kitchen"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", index, name="index"),
 
     path(
         "dishes/",
-        views.DishListView.as_view(),
+        DishListView.as_view(),
         name="dish-list",
     ),
     path(
         "dishes/<int:pk>/",
-        views.DishDetailView.as_view(),
+        DishDetailView.as_view(),
         name="dish-detail",
     ),
     path(
         "cooks/",
-        views.CookListView.as_view(),
+        CookListView.as_view(),
         name="cook-list",
     ),
     path(
         "cooks/<int:pk>/",
-        views.CookDetailView.as_view(),
+        CookDetailView.as_view(),
         name="cook-detail",
     ),
     path(
         "dish_types/",
-        views.DishTypeListView.as_view(),
+        DishTypeListView.as_view(),
         name="dish_type-list",
     ),
 ]
--- kitchen/views.py
+++ kitchen/views.py
@@ -3,40 +3,48 @@ from django.shortcuts import render
 from django.views import generic
 from kitchen.models import DishType, Cook, Dish
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
+
+@login_required
 def index(request):
     num_dish_type = DishType.objects.count()
     num_cook = Cook.objects.count()
     num_dish = Dish.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     context = {
         "num_dish_type": num_dish_type,
         "num_cook": num_cook,
         "num_dish": num_dish,
+        "num_visits": num_visits + 1
     }
 
     return render(request, "kitchen/index.html", context)
 
-class DishTypeListView(generic.ListView):
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
 
     model = DishType
     queryset = DishType.objects.all().order_by("name")
     paginate_by = 7
 
 
-class DishListView(generic.ListView):
+class DishListView(LoginRequiredMixin, generic.ListView):
 
     model = Dish
     queryset = Dish.objects.select_related("dish_type").all()
     paginate_by = 7
 
 
-class DishDetailView(generic.DetailView):
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
 
     model = Dish
 
 
-class CookListView(generic.ListView):
+class CookListView(LoginRequiredMixin, generic.ListView):
 
     model = Cook
     paginate_by = 7
@@ -45,4 +53,4 @@ class CookListView(generic.ListView):
 class CookDetailView(generic.DetailView):
 
     model = Cook
-    queryset = Cook.objects.prefetch_related("dish__dish_type").all()
+    queryset = Cook.objects.prefetch_related("dishes__dish_type").all()
